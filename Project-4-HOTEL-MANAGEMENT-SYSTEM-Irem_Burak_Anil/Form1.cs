b0a31ed47394dd11453bd134def9706e8f24a640
byanigli <byanigli@gmail.com>
2018-03-15 19:12:05 +0300

    Proje dosyası ekle.

---
+++ Project-4-HOTEL-MANAGEMENT-SYSTEM-Irem_Burak_Anil/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_4_HOTEL_MANAGEMENT_SYSTEM_Irem_Burak_Anil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project-4-HOTEL-MANAGEMENT-SYSTEM-Irem_Burak_Anil/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_4_HOTEL_MANAGEMENT_SYSTEM_Irem_Burak_Anil
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
